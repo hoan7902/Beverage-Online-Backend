3e7a741be02c8821c4b07e3831bfc1457750272e
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-10-02 13:59:37 +0700

    feat: product controller

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const user = require('./user');
+const product = require('./product');
 const route = (app) => {
     app.use('/user', user);
+    app.use('/product', product);
 };
 module.exports = route;

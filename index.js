1043c76c446cc1bd327342cec110992243a7ab8c
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-10-07 20:26:54 +0700

    chore: test vercel

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose.connect(dbURL, {
     useUnifiedTopology: true,
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Success');
 });
 

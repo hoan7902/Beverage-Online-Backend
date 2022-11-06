9b4d66ea820ddb10560ae302c6159dafb41e8f6c
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-11-07 01:12:00 +0700

    feat: cart redis

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,18 @@ const product = require('./product');
 const order = require('./order');
 const category = require('./category');
 const topping = require('./topping');
+const cart = require('./cart');
 const route = (app) => {
-    app.use('/user', user);
-    app.use('/product', product);
-    app.use('/order', order);
-    app.use('/category', category);
+  app.use('/user', user);
+  app.use('/product', product);
+  app.use('/order', order);
+  app.use('/category', category);
 
-    app.use('/topping', topping);
-    app.use('/', (req, res) => {
-        res.status(200).send({ message: 'Success to connect alo' });
-    });
+  app.use('/topping', topping);
+
+  app.use('/cart', cart);
+  app.use('/', (req, res) => {
+    res.status(200).send({ message: 'Success to connect alo' });
+  });
 };
 module.exports = route;

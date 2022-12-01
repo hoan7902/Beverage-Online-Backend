0f8f492a605b3441c553f42db73e381a4ad7b95f
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-12-02 00:50:15 +0700

    fix: 0rder

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -1,78 +1,81 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema(
-    {
-        userId: {
-            type: String,
-            required: true,
-        },
-        phoneNumber: {
-            required: true,
-            type: String,
-        },
-        userName: {
-            required: true,
-            type: String,
-        },
-        listProduct: [
-            {
-                productId: {
-                    type: String,
-                    required: true,
-                },
-                name: {
-                    type: String,
-                    required: true,
-                },
-                price: {
-                    type: Number,
-                    required: true,
-                },
-                image: {
-                    type: String,
-                    required: true,
-                },
-                quantity: {
-                    type: Number,
-                    required: true,
-                },
-                popular: {
-                    type: Number,
-                    required: true,
-                },
-                toppings: [
-                    {
-                        name: String,
-                        price: Number,
-                        image: String,
-                        quantity: Number,
-                    },
-                ],
-            },
-        ],
-        totalPrice: {
-            required: true,
-            type: Number,
-        },
-        isPay: {
-            required: true,
-            type: Boolean,
-        },
-        status: {
-            default: 'pending',
-            type: String,
-        },
-        address: {
-            required: true,
-            type: String,
-        },
-        description: {
-            type: String,
-            default: '',
-        },
+  {
+    userId: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    phoneNumber: {
+      required: true,
+      type: String,
+    },
+    userName: {
+      required: true,
+      type: String,
+    },
+    listProduct: [
+      {
+        productId: {
+          type: String,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        popular: {
+          type: Number,
+          required: true,
+        },
+        toppings: [
+          {
+            name: String,
+            price: Number,
+            image: String,
+            quantity: {
+              type: Number,
+              default: 1,
+            },
+          },
+        ],
+      },
+    ],
+    totalPrice: {
+      required: true,
+      type: Number,
+    },
+    isPay: {
+      required: true,
+      type: Boolean,
+    },
+    status: {
+      default: 'pending',
+      type: String,
+    },
+    address: {
+      required: true,
+      type: String,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 const OrderModel = mongoose.model('Order', schema);
 module.exports = OrderModel;
